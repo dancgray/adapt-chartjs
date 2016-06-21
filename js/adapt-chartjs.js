a7ae220815be12e4e2dd7371e44a7a60ffaa4710
Dan Gray <dan@sinensis.co.uk>
2016-06-21 20:53:58 +0100

    Rename to chartjs and use correct library file

--- js/adapt-chartjs.js
+++ js/adapt-chartjs.js
@@ -1,9 +1,10 @@
-define(function(require) {
+define([
+  'coreViews/componentView',
+  'coreJS/adapt',
+  './Chart.min'
+], function(ComponentView, Adapt, Chart){
 
-    var ComponentView = require('coreViews/componentView');
-    var Adapt = require('coreJS/adapt');
-
-    var Chart = ComponentView.extend({
+    var ChartJS = ComponentView.extend({
 
         events: {
 
@@ -13,7 +14,6 @@ define(function(require) {
             this.listenTo(Adapt, 'device:resize', this.onScreenSizeChanged);
             this.listenTo(Adapt, 'device:changed', this.onDeviceChanged);
             this.listenTo(Adapt, 'accessibility:toggle', this.onAccessibilityToggle);
-
             this.checkIfResetOnRevisit();
         },
 
@@ -23,6 +23,46 @@ define(function(require) {
 
 
         setupChart: function() {
+            var ctx = $("#myChart");
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    },
+                    responsive: true
+                }
+            });
+
+            this.setReadyStatus();
 
         },
 
@@ -85,8 +125,8 @@ define(function(require) {
 
     });
 
-    Adapt.register('chart', Chart);
+    Adapt.register('chartjs', ChartJS);
 
-    return Chart;
+    return ChartJS;
 
 });
